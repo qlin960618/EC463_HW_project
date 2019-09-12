bbffabb0328a0882973a8c9123d893f12f206763
qlin1806 <qlin1806@gmail.com>
2019-09-12 05:27:29 +0100

    sept18 final

--- HW_project_template/automate.sh
+++ HW_project_template/automate.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
+echo "initialized"
+sleep 5
+echo "script initialized"
 SEG_LEN=30
 N_SEG=10
+path_prefix="/home/pi/HW_Project/HW_project_template"
+
+echo "removing old data"
+rm -r $path_prefix/data_*
+
+echo "begin collection"
 
 #record segment
 for i in $(seq 0 $N_SEG); do
 	echo "recording "$i
-	python3 CaptureMotion.py "./data_"$i  $SEG_LEN -fps 30
+	python3 /home/pi/HW_Project/HW_project_template/CaptureMotion.py $path_prefix"/data_"$i  $SEG_LEN -fps 30
 	sleep 0.5
 	if [ $i -ne 0 ]; then
-		python3 CountMotion.py "./data_"$((i-1))"/motion.h5" "dy" > "./data_"$((i-1))"/dy.txt" &
-		python3 CountMotion.py "./data_"$((i-1))"/motion.h5" "dx" > "./data_"$((i-1))"/dx.txt" &
+		python3 /home/pi/HW_Project/HW_project_template/CountMotion.py $path_prefix"/data_"$((i-1))"/motion.h5" "dy" > $path_prefix"/data_"$((i-1))"/dy.txt" &
+		python3 /home/pi/HW_Project/HW_project_template/CountMotion.py $path_prefix"/data_"$((i-1))"/motion.h5" "dx" > $path_prefix"/data_"$((i-1))"/dx.txt" &
 	fi
 done
 
 #wrapup
-python3 CountMotion.py "./data_"$((N_SEG))"/motion.h5" "dy" > "./data_"$((N_SEG))"/dy.txt" &
-python3 CountMotion.py "./data_"$((N_SEG))"/motion.h5" "dx" > "./data_"$((N_SEG))"/dx.txt" &
+python3 /home/pi/HW_Project/HW_project_template/CountMotion.py $path_prefix"/data_"$((N_SEG))"/motion.h5" "dy" > $path_prefix"/data_"$((N_SEG))"/dy.txt" &
+python3 /home/pi/HW_Project/HW_project_template/CountMotion.py $path_prefix"/data_"$((N_SEG))"/motion.h5" "dx" > $path_prefix"/data_"$((N_SEG))"/dx.txt" &
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # EC463_HW_project
 Repository for Senior Design Hardware project
+
+the script is in the HW_project_template. To initialize, run automate.sh
